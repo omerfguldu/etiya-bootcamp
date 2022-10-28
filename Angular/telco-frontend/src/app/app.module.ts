411b0a973dac1e3a26899f05a15ab1b1c5853e60
gulduomer <43722346+omerfguldu@users.noreply.github.com>
2022-10-28 10:14:21 +0300

    form icinde gelen veri reactive forms yaklasimi kullanilarak listeye ekleni. etiya-telco-project projesi olusturuldu.

--- Angular/telco-frontend/src/app/app.module.ts
+++ Angular/telco-frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +15,7 @@ import { ProductListComponent } from './components/product-list/product-list.com
     ListviewComponent,
     ProductListComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   exports: [],
   providers: [],
   bootstrap: [AppComponent],
--- Angular/telco-frontend/src/app/components/product-list/product-list.component.ts
+++ Angular/telco-frontend/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-list',
@@ -6,20 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-  productInputValue: string;
   products: string[] = [];
+  productForm: FormGroup;
 
   constructor() {
     this.fillProducts();
+  }
+
+  createProductForm(): void {
+    this.productForm = new FormGroup({
+      productData: new FormGroup({
+        productName: new FormControl(null, Validators.required),
+      }),
+    });
   }
 
   fillProducts(): void {
     this.products = ['Product 1', 'Product 2', 'Product 3'];
   }
 
-  onAddProduct(): void {
-    this.products.push(this.productInputValue);
-    this.productInputValue = '';
+  onSubmit(): void {
+    const productValue: string = this.productForm.get(
+      'productData.productName'
+    ).value;
+    this.addProduct(productValue);
+    this.productForm.reset();
+  }
+
+  addProduct(value: string): void {
+    this.products.push(value);
   }
 
   onDeleteProduct(productIndex: number): void {
@@ -28,5 +44,7 @@ export class ProductListComponent implements OnInit {
     );
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.createProductForm();
+  }
 }
